6de51804303857d260123d1d0a2d1a16af0c9812
Lucas Nordqvist <46131@gymnasium.ax>
2023-01-26 10:20:32 +0200

    mer grejer

--- listsite/settings.py
+++ listsite/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'listsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD':'tOx83in0WxLme9IkGnys',
+        'HOST':'containers-us-west-71.railway.app',
+        'PORT':'7714',
+        
     }
 }
 
